a899187c75691aa978d1e394de26d7c260f187fe
SasinduPrasad <sasinduprasad2002@gmail.com>
2025-03-14 22:49:04 +0530

    Create HomeScreen.kt

---
+++ app/src/main/java/com/sasinduprasad/snapzzle/home/HomeScreen.kt
@@ -0,0 +1,173 @@
+package com.sasinduprasad.snapzzle.home
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.util.Log
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture.OnImageCapturedCallback
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Cameraswitch
+import androidx.compose.material.icons.filled.Photo
+import androidx.compose.material.icons.filled.PhotoCamera
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.sasinduprasad.snapzzle.CameraPreview
+import com.sasinduprasad.snapzzle.ImageView
+import com.sasinduprasad.snapzzle.MainViewModel
+import com.sasinduprasad.snapzzle.R
+
+@Composable
+fun HomeScreen(onNavigateToCreate:()->Unit) {
+
+    val isDarkMode = isSystemInDarkTheme()
+    val scrollState = rememberScrollState()
+
+    Scaffold (
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {onNavigateToCreate()},
+                containerColor = MaterialTheme.colorScheme.surface,
+                contentColor = MaterialTheme.colorScheme.tertiary
+            ) {
+                Icon(Icons.Filled.Add, "floating action button.")
+            }
+        }
+    )  { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(padding)
+                .padding(16.dp)
+                .verticalScroll(state = scrollState)
+        ) {
+
+            Image(
+                painter = painterResource(
+                    id = if (isDarkMode) R.drawable.snapzzle_logo else {
+                        R.drawable.snapzzle_logo_light
+                    }
+                ),
+                contentDescription = "snapzzle_logo"
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+                Text(
+                    text = "My Puzzles",
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colorScheme.tertiary,
+                    fontSize = 16.sp
+                )
+
+                LazyRow {
+                    repeat(5) {
+                        item {
+                            ElevatedCard(
+                                colors = CardDefaults.cardColors(
+                                    containerColor = MaterialTheme.colorScheme.secondary,
+                                ),
+                                elevation = CardDefaults.cardElevation(
+                                    defaultElevation = 6.dp
+                                ),
+                                modifier = Modifier
+                                    .size(width = 200.dp, height = 280.dp)
+                            ) {
+                                //puzzle preview
+                            }
+                            Spacer(modifier = Modifier.width(16.dp))
+                        }
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+                Text(
+                    text = "Challenges",
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colorScheme.tertiary,
+                    fontSize = 16.sp
+                )
+
+                LazyRow {
+                    repeat(5) {
+                        item {
+                            ElevatedCard(
+                                colors = CardDefaults.cardColors(
+                                    containerColor = MaterialTheme.colorScheme.secondary,
+                                ),
+                                elevation = CardDefaults.cardElevation(
+                                    defaultElevation = 6.dp
+                                ),
+                                modifier = Modifier
+                                    .size(width = 200.dp, height = 280.dp)
+                            ) {
+                                //puzzle preview
+                            }
+                            Spacer(modifier = Modifier.width(16.dp))
+                        }
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+
+
+        }
+
+    }
+}
+
