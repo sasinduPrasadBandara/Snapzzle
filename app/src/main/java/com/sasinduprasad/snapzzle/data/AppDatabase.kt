7a3a0bcda0dd03f4558ff95b95d3481e924a57db
SasinduPrasad <sasinduprasad2002@gmail.com>
2025-03-16 08:32:14 +0530

    Update AppDatabase.kt

--- app/src/main/java/com/sasinduprasad/snapzzle/data/AppDatabase.kt
+++ app/src/main/java/com/sasinduprasad/snapzzle/data/AppDatabase.kt
@@ -3,18 +3,22 @@ package com.sasinduprasad.snapzzle.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.sasinduprasad.snapzzle.data.Bitmap.BitmapData
+import com.sasinduprasad.snapzzle.data.Bitmap.BitmapDataDao
+import com.sasinduprasad.snapzzle.data.Bitmap.PuzzleWithBitmaps
 import com.sasinduprasad.snapzzle.data.puzzle.Puzzle
 import com.sasinduprasad.snapzzle.data.puzzle.PuzzleDao
 import com.sasinduprasad.snapzzle.data.user.User
 import com.sasinduprasad.snapzzle.data.user.UserDao
 
 @Database(
-    entities = [Puzzle::class, User::class],
+    entities = [Puzzle::class, User::class,BitmapData::class],
     version = 1,
     exportSchema = false
 )
-@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
     abstract fun puzzleDao(): PuzzleDao
+    abstract fun bitmapDataDao(): BitmapDataDao
+
 }
