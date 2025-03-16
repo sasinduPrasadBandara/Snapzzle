7aa272c985fa853d53046f97a41f2ccd698685ef
SasinduPrasad <sasinduprasad2002@gmail.com>
2025-03-16 08:32:40 +0530

    Update PuzzleDao.kt

--- app/src/main/java/com/sasinduprasad/snapzzle/data/puzzle/PuzzleDao.kt
+++ app/src/main/java/com/sasinduprasad/snapzzle/data/puzzle/PuzzleDao.kt
@@ -4,6 +4,8 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Transaction
+import com.sasinduprasad.snapzzle.data.Bitmap.PuzzleWithBitmaps
 
 @Dao
 interface PuzzleDao {
@@ -11,12 +13,20 @@ interface PuzzleDao {
     @Query("SELECT * FROM puzzle")
     fun getAll(): List<Puzzle>
 
+    @Transaction
     @Query("SELECT * FROM puzzle WHERE pid = :puzzleId")
-    suspend fun getById(puzzleId:Long):Puzzle
+    suspend fun getPuzzleWithBitmapsById(puzzleId: Long): PuzzleWithBitmaps
+
+    @Transaction
+    @Query("SELECT * FROM puzzle")
+    suspend fun getPuzzlesWithBitmaps(): List<PuzzleWithBitmaps>
 
     @Insert
-    suspend fun insert(puzzle: Puzzle)
+    suspend fun insert(puzzle: Puzzle): Long
 
-    @Delete
-    suspend fun delete(puzzle: Puzzle)
+    @Query("UPDATE puzzle SET duration = :duration, is_win = 1 WHERE pid = :puzzleId")
+    suspend fun update(duration: Int, puzzleId: Long)
+
+    @Query("DELETE FROM puzzle WHERE pid =:puzzleId")
+    suspend fun delete(puzzleId: Long)
 }
